87b5413c9907aae4162c1b3c5ec2397f5397e996
ashmore <ashmore@dc-energy.com>
2025-04-16 12:51:13 -0400

    accidently put Battle.h in Enemy.h. Now fixed

--- include/Enemy.h
+++ include/Enemy.h
@@ -1,27 +1,18 @@
 #pragma once
 #include <string>
-#include "Weapon.h"
-#include "Item.h"
 
 class Enemy {
 public:
-    Enemy(const std::string& name, int health, int attack, int defense, int agility, Weapon weapon);
-    
+    Enemy(const std::string& name, int health, int damage);
+
     std::string getName() const;
     int getHealth() const;
-    int getAttack() const;
-    int getDefense() const;
-    int getAgility() const;
-    Weapon getWeapon() const;
-    
-    void takeDamage(int damage);
-    bool isAlive() const;
+    int getDamage() const;
+
+    void takeDamage(int amount);
 
 private:
     std::string name;
     int health;
-    int attack;
-    int defense;
-    int agility;
-    Weapon weapon;
+    int damage;
 };
